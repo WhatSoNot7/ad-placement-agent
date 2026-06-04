a674b49f88b84da2c5feccafe571fa0e3bcb7f4e
seraleshcherbakov <SerAleShcherbakov@beeline.ru>
2026-06-04 09:58:14 +0300

    опечатка

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -2,7 +2,7 @@ from src.tools.plan_db import query_plan_db, save_corrections_to_db, save_final_
 from src.tools.excel_export import export_plan_to_excel
 from src.tools.validate_corrections import validate_corrections_file
 from src.tools.notifications import send_notification
-from src.tools.deadline import get_deadline_info
+from src.tools.deadlines import get_deadline_info
 
 __all__ = [
     "query_plan_db",
